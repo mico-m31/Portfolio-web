ecfb09d3ab43037330cadeff76d65ec911d4816f
mico-m31 <mico.martin@student.president.ac.id>
2025-07-27 23:20:54 +0700

    automation test part6_last_real

--- frontend/src/templates/About.jsx
+++ frontend/src/templates/About.jsx
@@ -9,6 +9,8 @@ function About(){
         <p>Test yang kelima kalinya</p>
         <p className="text-white">Automation test</p>
         <p> TEst LAgi</p>
+        <p>TEst Lagi Wak</p>
+            className="bg-purple-600 text-white px-4 py-2 sm:px-
         </>
     )
 }
