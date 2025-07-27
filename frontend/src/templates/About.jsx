c9a71838d56805aaa1f4acf6ff1ee3040a1240f6
mico-m31 <mico.martin@student.president.ac.id>
2025-07-27 23:32:26 +0700

    removing content at about page

--- frontend/src/templates/About.jsx
+++ frontend/src/templates/About.jsx
@@ -1,17 +1,6 @@
 function About(){
     return (
-        <>
-        <p className="text-white">Automation test</p>
-        <p> TEst LAgi</p>
-        <p className="text-white">Automation test</p>
-        <p> TEst LAgi</p>   
-        <p>TEst Lagi Wak</p>
-        <p>Test yang kelima kalinya</p>
-        <p className="text-white">Automation test</p>
-        <p> TEst LAgi</p>
-        <p>TEst Lagi Wak</p>
-            className="bg-purple-600 text-white px-4 py-2 sm:px-
-        </>
+      
     )
 }
 
